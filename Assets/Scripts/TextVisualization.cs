372dbc7a2a1a2ba1ae9ccceabc89c29e407b1fb2
mikepmerritt <mikepmerritt@gmail.com>
2021-09-29 06:28:16 -0400

    made text visualization hexagonal and added sprites for later

--- Assets/Scripts/TextVisualization.cs
+++ Assets/Scripts/TextVisualization.cs
@@ -6,32 +6,47 @@ public class TextVisualization : MonoBehaviour
 {
     public HexGrid Grid;
     public int GridRadius;
-    public string Output;
+    private string Output;
 
     void Start()
     {
+        // set up an empty hex grid
         Grid = new HexGrid(GridRadius);
+
+        // change the rightmost hex and hex top left from center to open
+        Grid.GetHexAtPosition(3, -3, 0).isOpen = true;
+        Grid.GetHexAtPosition(-1, 0, 1).isOpen = true;
+
+        // print the hex grid
         Output = "";
-        for (int x = -Grid.OuterRadius; x <= Grid.OuterRadius; x++)
+        int SpaceBuffer = GridRadius;
+
+        for (int z = Grid.OuterRadius; z >= -Grid.OuterRadius; z--)
         {
-            for (int y = -Grid.OuterRadius; y <= Grid.OuterRadius; y++)
+            for (int i = Mathf.Abs(SpaceBuffer); i > 0; i--)
             {
-                for (int z = Grid.OuterRadius; z >= -Grid.OuterRadius; z--)
+                Output += "  ";
+            }
+
+            for (int y = Grid.OuterRadius; y >= -Grid.OuterRadius; y--)
+            {
+                for (int x = Grid.OuterRadius; x >= -Grid.OuterRadius; x--)
                 {
                     if (x + y + z == 0) // only get cells that will be part of the hex grid
                     {
                         if(Grid.GetHexAtPosition(x, y, z).isOpen)
                         {
-                            Output += "O";
+                            Output += "O ";
                         }
                         else
                         {
-                            Output += "X";
+                            Output += "X ";
                         }
                     }
                 }
             }
             Output += "\n";
+            SpaceBuffer--;
         }
         Debug.Log(Output);
     }
